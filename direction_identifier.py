22037caa82ab198ed2d8e8ca2d8393f77ec28c4c
simone giovanardi <simogiovanardionline@gmail.com>
2025-01-26 21:23:39 +0100

    Some final adjustment

--- direction_identifier.py
+++ direction_identifier.py
@@ -1,16 +1,16 @@
 def direction_identifier(sum_1, sum_2):
     # 0° case
-    if sum_2 >= 9*sum_1:
+    if sum_2 >= 20*sum_1:
         return 0
     # 30° case
-    elif sum_2 > 1.6*sum_1 and sum_2 < 9*sum_1:
+    elif sum_2 > 1.6*sum_1 and sum_2 < 20*sum_1:
         return 30
     # 45° case
     elif sum_2 > (sum_1-(sum_1*0.6)) and sum_2 <= 1.6*sum_1:  
         return 45
     # 60° case
-    elif sum_1 > 1.6*sum_2 and sum_1 < 9*sum_2:
+    elif sum_1 > 1.6*sum_2 and sum_1 < 20*sum_2:
         return 60
     # 90° case
-    elif sum_1 >= 9*sum_2:
+    elif sum_1 >= 20*sum_2:
         return 90
